d143cc616ae584f6e32270e8e91d36de3adf8b46
shdx8 <elyasagus.29@gmail.com>
2021-03-15 21:18:01 +0700

    Update T3.php

--- W4/T3.php
+++ W4/T3.php
@@ -1,27 +1,35 @@
-<?php 
-class Tablet{
-    public $merk;
-    private $camera;
-    protected $memory='265GB';
+<?php
 
-    public function camera($item){
-        $camera=$this-> camera=$item;
-        return "dengan kualitas Kamera <b>".$camera."</b>";
-    }
-}
-class handphone extends Tablet{
-	private $handphone_baru;
-	public function beli_handphone(){
-	    $merk=$this -> merk;
-		$memory=$this -> memory;
-		$handphone_baru=$this -> handphone_baru = "Beli Hp Baru Merek <b>".$merk."</b> , dengan jumlah memory <b>".$memory."</b>";
-        return $handphone_baru;
-    }
+class Tablet {
+  public $merk;
+  public $tipe;
+  public $memory;
+
+  protected function getMerk(){
+    return $this->merk;
+  }
 }
 
-$Hp=new handphone();
-echo "<h1>Informasi : </h1>";
-$Hp-> merk='iPhone';
-$camera=$Hp-> camera('14 MP');
-$test=$Hp-> beli_handphone();
-echo $test.' '.$camera;
+class Handphone extends Tablet {
+  private function getMerkTipe() {
+    return "$this->merk $this->tipe";
+  }
+  public function beliHandphone(){
+    return "Beli handphone " . $this->getMerkTipe();
+  }
+  public function cekmemory(){
+    return "$this->tipe punya memory sebesar $this->memory GB";
+  }
+  public function Spek(){
+    return "Merk: $this->merk<br>
+    Tipe: $this->tipe<br>
+    memory: $this->memory GB<br>";
+  }
+}
+
+$hp = new Handphone();
+$hp->merk = "Xiaomi";
+$hp->tipe = "Redmi Note 9 Pro";
+$hp->memory = 8;
+
+echo $hp->Spek();
