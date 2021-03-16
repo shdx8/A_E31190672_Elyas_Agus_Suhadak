42218562744de7b21bbb5692dcea2840a1372bc2
shdx8 <elyasagus.29@gmail.com>
2021-03-16 16:37:11 +0700

    lil update

--- W6/application/controllers/hello.php
+++ W6/application/controllers/hello.php
@@ -1,6 +1,6 @@
 <?php 
 class Hello extends CI_Controller {
-    public function index() {
+    public function hello() {
         echo "<h2>Hello World CI!</h2>";
     
         $this->load->view('helloview');
