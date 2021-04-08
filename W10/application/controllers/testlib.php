71abc19ac1eb1de09430d6b6554a284569956c98
shdx8 <elyasagus.29@gmail.com>
2021-04-08 11:51:59 +0700

    add W10

---
+++ W10/application/controllers/testlib.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Teslib extends CI_Controller{
+  function __construct(){
+    parent::__construct();
+    $this->load->library('table');
+    $this->load->model('Mahasiswa_model');
+  }
+  public function index(){
+    $data['mahasiswa'] = $this->Mahasiswa_model->get_data();
+    $this->load->view('v_teslib', $data);
+  }
+}
