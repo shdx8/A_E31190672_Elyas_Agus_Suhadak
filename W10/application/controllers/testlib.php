72a82ed32df24275af42b56ad12f43229c419406
shdx8 <elyasagus.29@gmail.com>
2021-04-11 22:34:30 +0700

    W10 update

--- W10/application/controllers/testlib.php
+++ W10/application/controllers/testlib.php
@@ -1,13 +1,17 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-class Teslib extends CI_Controller{
-  function __construct(){
-    parent::__construct();
-    $this->load->library('table');
-    $this->load->model('Mahasiswa_model');
-  }
-  public function index(){
-    $data['mahasiswa'] = $this->Mahasiswa_model->get_data();
-    $this->load->view('v_teslib', $data);
-  }
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+class Testlib extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->library('table');
+		$this->load->model('Mahasiswa_model');
+	}
+
+	public function index()
+	{
+		$data['Mahasiswa']=$this->Mahasiswa_model->get_data();
+		$this->load->view('v_testlib', $data);
+	}
 }
+?>
--- W10/application/models/Mahasiswa_model.php
+++ W10/application/models/Mahasiswa_model.php
@@ -2,7 +2,7 @@
 class Mahasiswa_model extends CI_Model{
 	public function get_data(){
 		$data_mahasiswa = [
-			['nama'=>'Kim Jefry','prodi'=>'MIF'],
+			['nama'=>'Elyas','prodi'=>'MIF'],
 			['nama'=>'Ahmad Kurniawan','prodi'=>'TKK'],
 			['nama'=>'Budi Sudarsono','prodi'=>'TIF']
 		];
